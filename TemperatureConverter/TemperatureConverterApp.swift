5e72dd66440982597f41cd21623d865166e965cf
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-01-18 21:19:49 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Kiho Okazawa on 2023-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
